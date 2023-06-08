59cda7f0c35755327cd249eb5d93488720b2d2fa
R. S. Doiel <rsdoiel@gmail.com>
2023-06-08 12:25:50 -0700

    Quick Save

--- version.go
+++ version.go
@@ -6,13 +6,13 @@ import (
 
 const (
     // Version number of release
-    Version = "0.0.15"
+    Version = "0.0.16"
 
     // ReleaseDate, the date version.go was generated
     ReleaseDate = "2023-06-08"
 
     // ReleaseHash, the Git hash when version.go was generated
-    ReleaseHash = "b1012ef"
+    ReleaseHash = "70f0c02"
 
     LicenseText = `
 Copyright (c) 2023, Caltech All rights not granted herein are expressly
