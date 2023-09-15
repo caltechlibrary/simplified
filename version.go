6ea93c85334841e0c6a5ffd71228570814ba0473
R. S. Doiel <rsdoiel@gmail.com>
2023-09-15 10:04:29 -0700

    commiting to main

--- version.go
+++ version.go
@@ -6,13 +6,13 @@ import (
 
 const (
     // Version number of release
-    Version = "0.0.19"
+    Version = "0.0.20"
 
     // ReleaseDate, the date version.go was generated
     ReleaseDate = "2023-09-15"
 
     // ReleaseHash, the Git hash when version.go was generated
-    ReleaseHash = "0afcec1"
+    ReleaseHash = "ce8ee54"
 
     LicenseText = `
 Copyright (c) 2023, Caltech All rights not granted herein are expressly
